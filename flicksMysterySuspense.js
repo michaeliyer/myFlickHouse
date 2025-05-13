5ddddbf289da3d207f16aaac7af84e5b7f9e0f29
Michael Iyer <your-email@example.com>
2025-05-13 19:28:54 -0400

    Loading MSFlicks

--- flicksMysterySuspense.js
+++ flicksMysterySuspense.js
@@ -1,11 +1,91 @@
+// const flicksMysterySuspense = [
+//     {
+//         title: "3 Days of the Condor",
+//         year: 1975,
+//         genre: "Mystery Suspense",
+//         dropboxUrl:
+//           "https://www.dropbox.com/scl/fi/d7ae7mtthcr303pn74for/3_DAYS_OF_THE_CONDOR.mp4?rlkey=ujg7zogvriiw9hrf39o8eyz1r&raw=1",
+//         downloadUrl:
+//           "https://www.dropbox.com/scl/fi/d7ae7mtthcr303pn74for/3_DAYS_OF_THE_CONDOR.mp4?rlkey=ujg7zogvriiw9hrf39o8eyz1r&dl=1",
+//       },
+// ]
+
 const flicksMysterySuspense = [
     {
-        title: "3 Days of the Condor",
-        year: 1975,
-        genre: "Mystery Suspense",
-        dropboxUrl:
-          "https://www.dropbox.com/scl/fi/d7ae7mtthcr303pn74for/3_DAYS_OF_THE_CONDOR.mp4?rlkey=ujg7zogvriiw9hrf39o8eyz1r&raw=1",
-        downloadUrl:
-          "https://www.dropbox.com/scl/fi/d7ae7mtthcr303pn74for/3_DAYS_OF_THE_CONDOR.mp4?rlkey=ujg7zogvriiw9hrf39o8eyz1r&dl=1",
-      },
-]
+      title: "3 Days of the Condor",
+      year: 1975,
+      genre: "Mystery Suspense",
+      dropboxUrl: "https://www.dropbox.com/scl/fi/d7ae7mtthcr303pn74for/3_DAYS_OF_THE_CONDOR.mp4?rlkey=ujg7zogvriiw9hrf39o8eyz1r&raw=1",
+      downloadUrl: "https://www.dropbox.com/scl/fi/d7ae7mtthcr303pn74for/3_DAYS_OF_THE_CONDOR.mp4?rlkey=ujg7zogvriiw9hrf39o8eyz1r&dl=1"
+    },
+    {
+      title: "A Kiss Before Dying",
+      year: 1991,
+      genre: "Mystery Suspense",
+      dropboxUrl: "https://www.dropbox.com/scl/fi/kpeac4t9ymt3ztpsly8sl/A_KISS-_BEFORE-_DYING_1991.mp4?rlkey=o8etstwnn22gtlef1ry182bbb&raw=1",
+      downloadUrl: "https://www.dropbox.com/scl/fi/kpeac4t9ymt3ztpsly8sl/A_KISS-_BEFORE-_DYING_1991.mp4?rlkey=o8etstwnn22gtlef1ry182bbb&dl=1"
+    },
+    {
+      title: "Captive",
+      year: 2015,
+      genre: "Mystery Suspense",
+      dropboxUrl: "https://www.dropbox.com/scl/fi/0889nzs0ehi08n510mh7o/CAPTIVE.mp4?rlkey=yt2uu1kt4m05g2no137ofwvpm&raw=1",
+      downloadUrl: "https://www.dropbox.com/scl/fi/0889nzs0ehi08n510mh7o/CAPTIVE.mp4?rlkey=yt2uu1kt4m05g2no137ofwvpm&dl=1"
+    },
+    {
+      title: "The Conversation",
+      year: 1974,
+      genre: "Mystery Suspense",
+      dropboxUrl: "https://www.dropbox.com/scl/fi/b3zvb36sm5bzl912fpb5w/CONVERSATION.mp4?rlkey=bjhlv4n3epfu2jj7xf7rlshjq&raw=1",
+      downloadUrl: "https://www.dropbox.com/scl/fi/b3zvb36sm5bzl912fpb5w/CONVERSATION.mp4?rlkey=bjhlv4n3epfu2jj7xf7rlshjq&dl=1"
+    },
+    {
+      title: "Inception",
+      year: 2010,
+      genre: "Mystery Suspense",
+      dropboxUrl: "https://www.dropbox.com/scl/fi/wxe5mn7n1u50d44t5bd9j/INCEPTION.mp4?rlkey=n0z4q9rppcju8qh0e81931inp&raw=1",
+      downloadUrl: "https://www.dropbox.com/scl/fi/wxe5mn7n1u50d44t5bd9j/INCEPTION.mp4?rlkey=n0z4q9rppcju8qh0e81931inp&dl=1"
+    },
+    {
+      title: "Midnight In The Garden of Good And Evil",
+      year: 1994,
+      genre: "Mystery Suspense",
+      dropboxUrl: "https://www.dropbox.com/scl/fi/evhcr31th3crldgtlvvr6/MIDNIGHT_GARDEN_GOOD_EVIL.mp4?rlkey=r1q78nl3ii7o6fukuor2rga6c&raw=1",
+      downloadUrl: "https://www.dropbox.com/scl/fi/evhcr31th3crldgtlvvr6/MIDNIGHT_GARDEN_GOOD_EVIL.mp4?rlkey=r1q78nl3ii7o6fukuor2rga6c&dl=1"
+    },
+    {
+      title: "Ripley's Game",
+      year: 2002,
+      genre: "Mystery Suspense",
+      dropboxUrl: "https://www.dropbox.com/scl/fi/2cejguoqg261isngh0m2f/RIPLEY-S_GAME.mp4?rlkey=sn1ssjwxeatxwtgzjpd3g57y8&raw=1",
+      downloadUrl: "https://www.dropbox.com/scl/fi/2cejguoqg261isngh0m2f/RIPLEY-S_GAME.mp4?rlkey=sn1ssjwxeatxwtgzjpd3g57y8&dl=1"
+    },
+    {
+      title: "Shallow Grave",
+      year: 1994,
+      genre: "Mystery Suspense",
+      dropboxUrl: "https://www.dropbox.com/scl/fi/h16p0t1wazr0gd69rqxlr/SHALLOW_GRAVE.mp4?rlkey=v8gnovbl1mej4aejsdiw1q0wt&raw=1",
+      downloadUrl: "https://www.dropbox.com/scl/fi/h16p0t1wazr0gd69rqxlr/SHALLOW_GRAVE.mp4?rlkey=v8gnovbl1mej4aejsdiw1q0wt&dl=1"
+    },
+    {
+      title: "The Stranger",
+      year: 1946,
+      genre: "Mystery Suspense",
+      dropboxUrl: "https://www.dropbox.com/scl/fi/x2abwyv5sg479s2yagfg1/STRANGER.mp4?rlkey=h9cpx757j6v3v9mgyrkzbomn9&raw=1",
+      downloadUrl: "https://www.dropbox.com/scl/fi/x2abwyv5sg479s2yagfg1/STRANGER.mp4?rlkey=h9cpx757j6v3v9mgyrkzbomn9&dl=1"
+    },
+    {
+      title: "Suicide Kings",
+      year: 1997,
+      genre: "Mystery Suspense",
+      dropboxUrl: "https://www.dropbox.com/scl/fi/dbafwqzp6dywdoxfys6d5/SUICIDE_KINGS.mp4?rlkey=4bbvby3fbxjaowtcefnaa2do5&raw=1",
+      downloadUrl: "https://www.dropbox.com/scl/fi/dbafwqzp6dywdoxfys6d5/SUICIDE_KINGS.mp4?rlkey=4bbvby3fbxjaowtcefnaa2do5&dl=1"
+    },
+    {
+      title: "Touch Of Evil",
+      year: 1958,
+      genre: "Mystery Suspense",
+      dropboxUrl: "https://www.dropbox.com/scl/fi/5lunpwua5h6s2xsjo5gdx/TOUCH_OF_EVIL.mp4?rlkey=gik6a772na1mbr4czb38ug8o9&raw=1",
+      downloadUrl: "https://www.dropbox.com/scl/fi/5lunpwua5h6s2xsjo5gdx/TOUCH_OF_EVIL.mp4?rlkey=gik6a772na1mbr4czb38ug8o9&dl=1"
+    }
+  ];
