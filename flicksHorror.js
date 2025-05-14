07f3b49688879ff38e7775e0f587ba1e25c1befe
Michael Iyer <your-email@example.com>
2025-05-13 20:17:22 -0400

    Loading Horror

--- flicksHorror.js
+++ flicksHorror.js
@@ -1,5 +1,26 @@
 const flicksHorror = [
   {
+    title: "28 Days Later (Full Screen)",
+    year: 2002,
+    genre: "Horror",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/oqwqe6znz9taskl09hwk1/28DAYS_LATER_Fullscreen.mp4?rlkey=z8bw6gomhllqbv5tw45vwkxy0&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/oqwqe6znz9taskl09hwk1/28DAYS_LATER_Fullscreen.mp4?rlkey=z8bw6gomhllqbv5tw45vwkxy0&dl=1"
+  },
+  {
+    title: "28 Weeks Later (Full Screen)",
+    year: 2007,
+    genre: "Horror",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/rabjbqc3cbrbnggsawtcw/28WEEKS_LATER_Fullscreen.mp4?rlkey=vdsrlw71hdkvfo2ebqabbi5fc&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/rabjbqc3cbrbnggsawtcw/28WEEKS_LATER_Fullscreen.mp4?rlkey=vdsrlw71hdkvfo2ebqabbi5fc&dl=1"
+  },
+  {
+    title: "30 Days Of Night",
+    year: 2007,
+    genre: "Horror",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/aasvsxmpfmgw4g8gq5ogc/30_DAYS_OF_NIGHT.mp4?rlkey=xm16v6nw93gpmhdbvgf0y93o4&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/aasvsxmpfmgw4g8gq5ogc/30_DAYS_OF_NIGHT.mp4?rlkey=xm16v6nw93gpmhdbvgf0y93o4&dl=1"
+  },
+  {    
     title: "1408",
     year: 2007,
     genre: "Horror",
@@ -7,6 +28,84 @@ const flicksHorror = [
       "https://www.dropbox.com/scl/fi/litc7i2xj0w1khe5r09a9/1408.mp4?rlkey=bqfkag30026cvbo91f3ox4a37&raw=1",
     downloadUrl:
       "https://www.dropbox.com/scl/fi/litc7i2xj0w1khe5r09a9/1408.mp4?rlkey=bqfkag30026cvbo91f3ox4a37&dl=1",
+  },
+  {
+    title: "The Amityville Horror",
+    year: 1977,
+    genre: "Horror",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/6b3qs1xsw727mgzo7fx2x/AMITYVILLE_HORROR.mp4?rlkey=b3np9de0j45t6mwkn0cnmihr0&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/6b3qs1xsw727mgzo7fx2x/AMITYVILLE_HORROR.mp4?rlkey=b3np9de0j45t6mwkn0cnmihr0&dl=1"
+  },
+  
+  {
+    title: "Audition",
+    year: 1999,
+    genre: "Horror",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/61nbf2r5cl0myg383z90m/AUDITION.mp4?rlkey=f6irc00ddvikou1lwjyfi27hr&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/61nbf2r5cl0myg383z90m/AUDITION.mp4?rlkey=f6irc00ddvikou1lwjyfi27hr&dl=1"
+  },
+  {
+    title: "Blair Witch Project",
+    year: 1999,
+    genre: "Horror",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/61nbf2r5cl0myg383z90m/AUDITION.mp4?rlkey=f6irc00ddvikou1lwjyfi27hr&raw=1",  // NOTE: Same link as Audition
+    downloadUrl: "https://www.dropbox.com/scl/fi/61nbf2r5cl0myg383z90m/AUDITION.mp4?rlkey=f6irc00ddvikou1lwjyfi27hr&dl=1"
+  },
+  {
+    title: "The Box",
+    year: 2009,
+    genre: "Horror",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/1jx5h7s00bp9y7ne87vug/BOX.mp4?rlkey=c9t9d7p3u4hrejhpyxpro1xa5&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/1jx5h7s00bp9y7ne87vug/BOX.mp4?rlkey=c9t9d7p3u4hrejhpyxpro1xa5&dl=1"
+  },
+  {
+    title: "Bram Stoker's Dracula",
+    year: 1992,
+    genre: "Horror",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/4ej9yz8c0q7a36j5cq8xt/BRAM_STOKER-S_DRACULA.mp4?rlkey=qst4oyps8a4ynxrbyipisjxgn&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/4ej9yz8c0q7a36j5cq8xt/BRAM_STOKER-S_DRACULA.mp4?rlkey=qst4oyps8a4ynxrbyipisjxgn&dl=1"
+  },
+  {
+    title: "Candyman",
+    year: 1992,
+    genre: "Horror",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/kihi007kyjguur9roqlko/CANDYMAN.mp4?rlkey=ft03xu3ravrccsdkti062w5zj&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/kihi007kyjguur9roqlko/CANDYMAN.mp4?rlkey=ft03xu3ravrccsdkti062w5zj&dl=1"
+  },
+  {
+    title: "Carrie",
+    year: 1976,
+    genre: "Horror",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/8yb5j9gv29skx49o7ymgf/CARRIE.mp4?rlkey=xdcfautxuych0ndh4tkol5ow4&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/8yb5j9gv29skx49o7ymgf/CARRIE.mp4?rlkey=xdcfautxuych0ndh4tkol5ow4&dl=1"
+  },
+  {
+    title: "Chernobyl Diaries",
+    year: 2012,
+    genre: "Horror",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/w7r6xjd4k3jgnyghqe93p/CHERNOBYL_DIARIES.mp4?rlkey=7c41edapmefvo2cjm5tv9ix5f&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/w7r6xjd4k3jgnyghqe93p/CHERNOBYL_DIARIES.mp4?rlkey=7c41edapmefvo2cjm5tv9ix5f&dl=1"
+  },
+  {
+    title: "Children Of The Damned",
+    year: 1964,
+    genre: "Horror",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/apsx74x090wri3d3fkisp/CHILDREN_OF_THE_DAMNED.mp4?rlkey=gwofi9sg7jlsde4r9mane8qwv&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/apsx74x090wri3d3fkisp/CHILDREN_OF_THE_DAMNED.mp4?rlkey=gwofi9sg7jlsde4r9mane8qwv&dl=1"
+  },
+  {
+    title: "The Children",
+    year: 2008,
+    genre: "Horror",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/wilhv8qhldygo05a1d00m/CHILDREN.mp4?rlkey=qhtiaoccvdpxauywgc9w2trrz&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/wilhv8qhldygo05a1d00m/CHILDREN.mp4?rlkey=qhtiaoccvdpxauywgc9w2trrz&dl=1"
+  },
+  {
+    title: "Christine",
+    year: 1983,
+    genre: "Horror",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/by8ltaanmt7mudq3lms8y/CHRISTINE.mp4?rlkey=3z32zg4jm6iwaj2ol2swy46an&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/by8ltaanmt7mudq3lms8y/CHRISTINE.mp4?rlkey=3z32zg4jm6iwaj2ol2swy46an&dl=1"
   },
   {
     title: "Trilogy of Terror",
