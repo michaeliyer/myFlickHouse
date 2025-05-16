06724088587e0c621081aa85fcd5086aab397d55
Michael Iyer <your-email@example.com>
2025-05-16 12:48:09 -0400

    SomeComedy loaded through Holy G.

--- flicksComedy.js
+++ flicksComedy.js
@@ -8,4 +8,197 @@ const flicksComedy = [
     downloadUrl:
       "https://www.dropbox.com/scl/fi/octavw6tprysmg1g3vrr1/3_AMIGOS.mp4?rlkey=85d3xzwm9885396gnr2cknlz3&dl=1   ",
   },
+
+  {
+    title: "10",
+    year: 1979,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/xs0v486vdqo3ey0pm5qlb/10.mp4?rlkey=dz7tbu1ajjhvv8vk2op9dw5ct&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/xs0v486vdqo3ey0pm5qlb/10.mp4?rlkey=dz7tbu1ajjhvv8vk2op9dw5ct&dl=1"
+  },
+  {
+    title: "Airplane",
+    year: 1980,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/h7smb1z9rf31xoty8kv3h/Airplane.mp4?rlkey=tr4yazfk7u1res1olqod5r21t&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/h7smb1z9rf31xoty8kv3h/Airplane.mp4?rlkey=tr4yazfk7u1res1olqod5r21t&dl=1"
+  },
+  {
+    title: "Almost Famous",
+    year: 2000,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/g1urev47s6xko4mq2bxvl/ALMOST_FAMOUS.mp4?rlkey=1ak98hy6ifv6wpntnsbn14avh&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/g1urev47s6xko4mq2bxvl/ALMOST_FAMOUS.mp4?rlkey=1ak98hy6ifv6wpntnsbn14avh&dl=1"
+  },
+  {
+    title: "Along Came Polly",
+    year: 2004,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/fh7rdgykffro5xtl1xqpx/ALONG_CAME_POLLY.mp4?rlkey=4hll7ydkgze5876qgh208z68r&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/fh7rdgykffro5xtl1xqpx/ALONG_CAME_POLLY.mp4?rlkey=4hll7ydkgze5876qgh208z68r&dl=1"
+  },
+  {
+    title: "Amazon Women On The Moon",
+    year: 1987,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/q39l44siv29wi4lel1jwe/AMAZON_WOMEN_ON_THE_MOON.mp4?rlkey=w6xj3eg6uatilkpn9cke3aorg&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/q39l44siv29wi4lel1jwe/AMAZON_WOMEN_ON_THE_MOON.mp4?rlkey=w6xj3eg6uatilkpn9cke3aorg&dl=1"
+  },
+  {
+    title: "American Graffiti",
+    year: 1973,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/4nicp0j5e6ukqd4g3uq7i/AMERICAN_GRAFFITI.mp4?rlkey=uqa6mwjlplyy2r3xtf8kp9tn4&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/4nicp0j5e6ukqd4g3uq7i/AMERICAN_GRAFFITI.mp4?rlkey=uqa6mwjlplyy2r3xtf8kp9tn4&dl=1"
+  },
+  {
+    title: "American Pie",
+    year: 1999,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/tcveo2vlo7jiya4ngg8a6/AMERICAN_PIE.mp4?rlkey=fe5230eai8f5cirvp48tvqi8d&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/tcveo2vlo7jiya4ngg8a6/AMERICAN_PIE.mp4?rlkey=fe5230eai8f5cirvp48tvqi8d&dl=1"
+  },
+  {
+    title: "Anchorman",
+    year: 2004,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/ny3c5s4fkfe5orb8q4lj1/ANCHORMAN.mp4?rlkey=uq717qdterexcgr9srgbcj6m8&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/ny3c5s4fkfe5orb8q4lj1/ANCHORMAN.mp4?rlkey=uq717qdterexcgr9srgbcj6m8&dl=1"
+  },
+  {
+    title: "Angel On My Shoulder",
+    year: 1946,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/pkv63gdoirwox9anrqcgk/ANGEL_ON_MY_SHOULDER.mp4?rlkey=vcou1yr8lo1mmdlqmojmrd6x7&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/pkv63gdoirwox9anrqcgk/ANGEL_ON_MY_SHOULDER.mp4?rlkey=vcou1yr8lo1mmdlqmojmrd6x7&dl=1"
+  },
+  {
+    title: "Arthur",
+    year: 1981,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/zrytlyqyh824csor8xjhl/ARTHUR.mp4?rlkey=vm1h4kojvxnna2pefu0e793uc&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/zrytlyqyh824csor8xjhl/ARTHUR.mp4?rlkey=vm1h4kojvxnna2pefu0e793uc&dl=1"
+  },
+  {
+    title: "As Good As It Gets",
+    year: 1997,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/q8ci5gbhuxkcg5s2wl9b6/AS_GOOD_AS_IT_GETS.mp4?rlkey=v6ujfl4ls5hmgiwxgzf98jp7f&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/q8ci5gbhuxkcg5s2wl9b6/AS_GOOD_AS_IT_GETS.mp4?rlkey=v6ujfl4ls5hmgiwxgzf98jp7f&dl=1"
+  },
+  {
+    title: "Benny And Joon",
+    year: 1993,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/7gi3721zr8ts8rdevaa57/BENNY_AND_JUNE.mp4?rlkey=pvwmrwjr7qju4g7aoe83fcp4a&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/7gi3721zr8ts8rdevaa57/BENNY_AND_JUNE.mp4?rlkey=pvwmrwjr7qju4g7aoe83fcp4a&dl=1"
+  },
+  {
+    title: "Best In Show",
+    year: 2000,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/k4rkaxjo1yhe37ctr1ldw/BEST_IN_SHOW.mp4?rlkey=7524y48l80p9v3pamq804sd9c&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/k4rkaxjo1yhe37ctr1ldw/BEST_IN_SHOW.mp4?rlkey=7524y48l80p9v3pamq804sd9c&dl=0"
+  },
+    {
+    title: "Borat",
+    year: 2006,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/ainlxz72wfjapdzeok97n/BORAT.mp4?rlkey=v5t3qn2jq3bi8ghe4t6pbwrjx&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/ainlxz72wfjapdzeok97n/BORAT.mp4?rlkey=v5t3qn2jq3bi8ghe4t6pbwrjx&dl=1"
+  },
+  {
+    title: "Bottle Rocket",
+    year: 1996,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/332idfubk4oe53gg4z6li/BOTTLE_ROCKET.mp4?rlkey=yttx8v1nnx4v0i35tjus825r7&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/332idfubk4oe53gg4z6li/BOTTLE_ROCKET.mp4?rlkey=yttx8v1nnx4v0i35tjus825r7&dl=1"
+  },
+  {
+    title: "The Break-Up",
+    year: 2006,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/y92xgx7fy817ne5otvxt9/BREAK_UP.mp4?rlkey=aso7ukrj4uavlcdndtuqt709z&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/y92xgx7fy817ne5otvxt9/BREAK_UP.mp4?rlkey=aso7ukrj4uavlcdndtuqt709z&dl=1"
+  },
+  {
+    title: "Caddyshack",
+    year: 1980,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/y92xgx7fy817ne5otvxt9/BREAK_UP.mp4?rlkey=aso7ukrj4uavlcdndtuqt709z&raw=1", // ⚠️ Duplicate link as "Break-Up"
+    downloadUrl: "https://www.dropbox.com/scl/fi/y92xgx7fy817ne5otvxt9/BREAK_UP.mp4?rlkey=aso7ukrj4uavlcdndtuqt709z&dl=1"
+  },
+  {
+    title: "Christmas Vacation",
+    year: 1989,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/hfljrt995t5qplf2g7gc4/CHRISTMAS_VACATION.mp4?rlkey=rkhobb0pfy0nxbyab0ser4a2q&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/hfljrt995t5qplf2g7gc4/CHRISTMAS_VACATION.mp4?rlkey=rkhobb0pfy0nxbyab0ser4a2q&dl=1"
+  },
+  {
+    title: "The Curse Of The Pink Panther",
+    year: 1983,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/o8an6vk6bxa3ljqnl8vmc/CURSE_OF_THE_PINK_PANTHER.mp4?rlkey=rckcn6fqwpqu1ho02bob4d5of&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/o8an6vk6bxa3ljqnl8vmc/CURSE_OF_THE_PINK_PANTHER.mp4?rlkey=rckcn6fqwpqu1ho02bob4d5of&dl=1"
+  },
+  {
+    title: "Dazed and Confused",
+    year: 1993,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/9hf40cbvkpbuyzqlq4p4z/DAZED_AND_CONFUSED.mp4?rlkey=8fq07z4qmnd9074wxve6k6p97&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/9hf40cbvkpbuyzqlq4p4z/DAZED_AND_CONFUSED.mp4?rlkey=8fq07z4qmnd9074wxve6k6p97&dl=1"
+  },
+  {
+    title: "Elf",
+    year: 2003,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/9hf40cbvkpbuyzqlq4p4z/DAZED_AND_CONFUSED.mp4?rlkey=8fq07z4qmnd9074wxve6k6p97&raw=1", // ⚠️ Same link as "Dazed and Confused"
+    downloadUrl: "https://www.dropbox.com/scl/fi/9hf40cbvkpbuyzqlq4p4z/DAZED_AND_CONFUSED.mp4?rlkey=8fq07z4qmnd9074wxve6k6p97&dl=1"
+  },
+  {
+    title: "European Vacation",
+    year: 1985,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/as7sjsr0kk9j6bigu9q14/EUROPEAN_VACATION.mp4?rlkey=yti25wvnd1ysuqc6jgsqomnnp&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/as7sjsr0kk9j6bigu9q14/EUROPEAN_VACATION.mp4?rlkey=yti25wvnd1ysuqc6jgsqomnnp&dl=1"
+  },
+  {
+    title: "Fast Times at Ridgemont High",
+    year: 1982,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/02gf6vv93wo0t16fgst44/FAST_TIMES_AT_RIDGEMONT_HIGH.mp4?rlkey=90gfkbttcwkfaau9ojd4ulq38&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/02gf6vv93wo0t16fgst44/FAST_TIMES_AT_RIDGEMONT_HIGH.mp4?rlkey=90gfkbttcwkfaau9ojd4ulq38&dl=1"
+  },
+  {
+    title: "Ferris Bueller's Day Off",
+    year: 1986,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/eqkihjrau32lnd1fnf0hs/FERRIS_BUELERS_DAY_OFF.mp4?rlkey=crlkdgllvvu1lmcgwpd1utz5h&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/eqkihjrau32lnd1fnf0hs/FERRIS_BUELERS_DAY_OFF.mp4?rlkey=crlkdgllvvu1lmcgwpd1utz5h&dl=1"
+  },
+  {
+    title: "The Flamingo Kid",
+    year: 1984,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/eqkihjrau32lnd1fnf0hs/FERRIS_BUELERS_DAY_OFF.mp4?rlkey=crlkdgllvvu1lmcgwpd1utz5h&raw=1", // ⚠️ Same link as "Ferris Bueller"
+    downloadUrl: "https://www.dropbox.com/scl/fi/eqkihjrau32lnd1fnf0hs/FERRIS_BUELERS_DAY_OFF.mp4?rlkey=crlkdgllvvu1lmcgwpd1utz5h&dl=1"
+  },
+  {
+    title: "Fletch",
+    year: 1985,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/iw4lo4tpi44h4j0wfrhin/FLETCH.mp4?rlkey=vemjme92dhwiirdyzc7wu53ze&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/iw4lo4tpi44h4j0wfrhin/FLETCH.mp4?rlkey=vemjme92dhwiirdyzc7wu53ze&dl=1"
+  },
+  {
+    title: "Monty Python and the Holy Grail",
+    year: 1975,
+    genre: "Comedy",
+    dropboxUrl: "https://www.dropbox.com/scl/fi/jwj7c5clxe3u4fv64sw0k/HOLY_GRAIL.mp4?rlkey=8imrrcfzht7y0295je2pkatgg&raw=1",
+    downloadUrl: "https://www.dropbox.com/scl/fi/jwj7c5clxe3u4fv64sw0k/HOLY_GRAIL.mp4?rlkey=8imrrcfzht7y0295je2pkatgg&dl=1"
+  }
+
+
+
 ];
