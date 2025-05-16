5971908fe156f1d20265398affa070e171e01bbc
Michael Iyer <your-email@example.com>
2025-05-16 16:28:51 -0400

    Blue Tango

--- flicksMusic.js
+++ flicksMusic.js
@@ -8,4 +8,13 @@ const flicksMusic = [
     downloadUrl:
       "https://www.dropbox.com/scl/fi/2bw2043lhnbsyal78xmxd/LASTWALTZ.mp4?rlkey=cylfpnjamnptdacqvafwyv2u4&dl=1",
   },
+  {
+    title: "Blue Tango - Josh Turner",
+    year: 2011,
+    genre: "Music",
+    dropboxUrl:
+      "https://www.dropbox.com/scl/fi/yrmhxdyz6esf7q14vvlwf/BlueTangoJosh.mp4?rlkey=r3lcqai6baa7ezfyal6bm7n4a&raw=1",
+    downloadUrl:
+      "https://www.dropbox.com/scl/fi/yrmhxdyz6esf7q14vvlwf/BlueTangoJosh.mp4?rlkey=r3lcqai6baa7ezfyal6bm7n4a&dl=0",
+  },
 ];
